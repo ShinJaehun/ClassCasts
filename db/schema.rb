062186ce0517b231066d3b2d22315fcd47992619
ShinJaehun <gunsmoke.shin@gmail.com>
2013-07-10 02:02:00 +0900

    Fixed index page:user registration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130708150900) do
+ActiveRecord::Schema.define(:version => 20130709150634) do
 
   create_table "casts", :force => true do |t|
     t.string   "title"
@@ -35,6 +35,7 @@ ActiveRecord::Schema.define(:version => 20130708150900) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at",                             :null => false
     t.datetime "updated_at",                             :null => false
+    t.string   "username"
   end
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
